23982f78407c1cf3ee4532100eef3f2a25eb2e65
Steven Smith <stesmi@gmail.com>
2025-09-25 17:29:32 +1000

    Send base64 images

--- asv-dark-sky/App.tsx
+++ asv-dark-sky/App.tsx
@@ -13,12 +13,22 @@ import { Image } from "expo-image";
 
 export type ViewMode = "camera" | "review";
 
+export type responseFormat = {
+  median_sky_brightness_dn: number;
+  sky_quality_meter: number;
+};
+
+const SERVER_URL =
+  "https://australia-southeast2-popkorn-472305.cloudfunctions.net/calculate_sky_brightness";
+
 export default function App() {
   const [permission, requestPermission] = useCameraPermissions();
   const ref = useRef<CameraView>(null);
   const [uris, setUris] = useState<string[]>([]);
   const [mode, setMode] = useState<CameraMode>("picture");
   const [viewMode, setViewMode] = useState<ViewMode>("camera");
+  const [SQM, setSQM] = useState<number | null>(null);
+  const [error, setError] = useState<string | null>(null);
   // const [facing, setFacing] = useState<CameraType>("back");
   // const [recording, setRecording] = useState(false);
 
@@ -43,6 +53,57 @@ export default function App() {
     setViewMode("review");
   };
 
+  const toBase64 = async (uri: string) => {
+    const response = await fetch(uri);
+    const blob = await response.blob();
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result);
+      reader.onerror = reject;
+      reader.readAsDataURL(blob);
+    });
+  };
+
+  const sendToServer = async () => {
+    // send uris to server
+    let lightFrame = uris[0];
+    let darkFrame = uris[1];
+    if (lightFrame && darkFrame) {
+      const response = await fetch(SERVER_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          light: lightFrame,
+          dark: darkFrame,
+          zero_point: 20.0,
+          exposure_time_s: 3,
+          metadata: {
+            location: "optional_location_info",
+            timestamp: "optional_timestamp",
+          },
+        }),
+      });
+
+      const responseFormat = {
+        median_sky_brightness_dn: 12.0,
+        sky_quality_meter: 18.494850021680094,
+      };
+
+      if (!response.ok) {
+        console.error("Failed to send images to server");
+        setSQM(null);
+        setError(
+          "Failed to get sky quality meter reading, could not send images to server"
+        );
+      } else {
+        const data: responseFormat = await response.json();
+        setSQM(data.sky_quality_meter);
+      }
+    }
+  };
+
   const renderPictures = (uris: string[]) => {
     return (
       <View>
@@ -53,14 +114,14 @@ export default function App() {
               source={{ uri }}
               contentFit="cover"
               style={{
-                width: 300,
-                height: 300,
+                width: 200,
+                height: 200,
                 borderRadius: "50%",
                 aspectRatio: 1,
               }}
             />
             <p style={{ textAlign: "center" }}>
-              {idx === 0 ? "Light" : "Dark"} Frame
+              {idx === 0 ? "Light" : "Dark"}
             </p>
           </div>
         ))}
@@ -70,8 +131,35 @@ export default function App() {
               setMode("picture");
               setViewMode("camera");
             }}
-            title={`Take ${uris.length > 0 ? "dark" : "light"} frame`}
+            title={`${uris.length === 1 ? "Shield the lenses and " : ""}Take ${
+              uris.length > 0 ? "dark" : "light"
+            } frame`}
           />
+        )}
+        {uris.length === 2 && (
+          <Button
+            onPress={() => {
+              sendToServer();
+            }}
+            title={`Check my sky quality`}
+          />
+        )}
+        {SQM && (
+          <Text style={{ textAlign: "center", marginTop: 20, fontSize: 18 }}>
+            Your sky quality meter reading is {SQM.toFixed(2)}
+          </Text>
+        )}
+        {error && (
+          <Text
+            style={{
+              textAlign: "center",
+              marginTop: 20,
+              fontSize: 18,
+              color: "red",
+            }}
+          >
+            {error}
+          </Text>
         )}
       </View>
     );
@@ -86,6 +174,7 @@ export default function App() {
           mode={mode}
           mute={true}
           responsiveOrientationWhenOrientationLocked
+          flash="off"
         />
         <View style={styles.shutterContainer}>
           <Pressable onPress={takePicture}>
